dc54e81c28e746a73d4752117bc4e8ea2a492d85
Yahia-Ibrahim <yahiaibrahime123@gmail.com>
2024-01-02 00:42:05 +0200

    fixed point modified

--- src/main/java/com/CSED26/Numercal/Project/Factory/Methods/Iterations/FixedPoint.java
+++ src/main/java/com/CSED26/Numercal/Project/Factory/Methods/Iterations/FixedPoint.java
@@ -10,13 +10,29 @@ public class FixedPoint extends Iterations {
     private double tol = 0.00001;
     private  int significantFigures = 5;
 
+    private Expressionn exp;
+    private double initialGuess;
+
     public static int convergedAfter = 0;
 
-    public FixedPoint(int maxIterations, int significantFigures, double tol) {
-        this.convergedAfter = 0;
+    public FixedPoint(double point, Expressionn exp, int maxIterations, int significantFigures, double tol) {
+        this.initialGuess = point;
+        this.exp = exp;
+        convergedAfter = 0;
         this.maxIterations = maxIterations;
         this.tol = tol;
         this.significantFigures = significantFigures;
+    }
+//    public FixedPoint(int maxIterations, int significantFigures, double tol) {
+//        this.convergedAfter = 0;
+//        this.maxIterations = maxIterations;
+//        this.tol = tol;
+//        this.significantFigures = significantFigures;
+//    }
+    public FixedPoint(double point, Expressionn exp) {
+        this.initialGuess = point;
+        this.exp = exp;
+        convergedAfter = 0;
     }
 
     @Override
@@ -37,7 +53,7 @@ public class FixedPoint extends Iterations {
 
     @Override
     public double getAnswers() {
-        return 0;
+        return iteration(this.initialGuess, this.exp);
     }
 
     @Override
@@ -48,9 +64,8 @@ public class FixedPoint extends Iterations {
     public static void main(String[] args) {
         Expressionn exp = new Expressionn();
         exp.addTerm("1+1/x");
-        //exp.addTerm(("-2"));
-        FixedPoint fp = new FixedPoint(100, 5, 0.00001);
-        System.out.println(fp.iteration(2, exp));
-        //System.out.println(exp.substitute(2));
+        FixedPoint fp = new FixedPoint(2, exp);
+        System.out.println(fp.getAnswers());
+        
     }
 }
