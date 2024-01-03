b8379e6c704d0b8ccfb68e97d5cef4f0c56c979e
zoaa3054 <zoaa3054@gmail.com>
2024-01-03 17:27:06 +0200

    .

--- src/main/java/com/CSED26/Numercal/Project/Controller.java
+++ src/main/java/com/CSED26/Numercal/Project/Controller.java
@@ -169,7 +169,7 @@ public class Controller {
         return solver.solveByFixedPoint(point, exp, maxIterations, significantFigures, tol);
     }
 
-    @GetMapping("/N1")
+    @GetMapping("/MN1")
     public double[] originalNewtonSolver(@RequestParam int MAX_ITERATIONS, @RequestParam double ea,
             @RequestParam double initialguess, @RequestParam int significantfigures, @RequestParam int m) {
         double[] answer = { solver.solveByONewton(1, m, MAX_ITERATIONS, ea, initialguess, significantfigures),
